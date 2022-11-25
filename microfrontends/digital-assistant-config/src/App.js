36fff6cd0f320e8abaddea6fd855b6f8a951dca4
Matteo Emanuele Minnai <m.minnai@entando.com>
2022-11-25 11:16:21 +0100

    Configuration WIP

--- microfrontends/digital-assistant-config/src/App.js
+++ microfrontends/digital-assistant-config/src/App.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
+import {Buffer} from "buffer";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    /*
+    let encoded;
+
+    if (props.name != null && props.name !== '' ) {
+      console.log("encoding", props);
+      encoded = Buffer.from(props.name, 'utf-8').toString('base64');
+      console.log("to", encoded);
+    } else {
+      console.log("using current value", props.name);
+      encoded = props.name;
+    }*/
     this.state = {
       name: props.name
     };
@@ -10,10 +22,22 @@ class App extends Component {
 
   handleChange = e => {
     const input = e.target;
+    const encoded = input.value;
+    let decoded;
+
+
+      decoded = Buffer.from(encoded, 'base64').toString('utf-8');
+      if (encoded !== '' && Buffer.from(decoded, 'utf-8').toString('base64') === encoded) {
+          // console.log('have valid configuration', decoded);
+      } else {
+        decoded = input.value;
+      }
+
     this.setState({
-      [input.name]: input.value,
+      [input.name]: decoded,
     });
   };
+
 
   render() {
     const { name } = this.state;
