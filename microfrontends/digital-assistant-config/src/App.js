39104c3fdb0f9bd922d046276f8d44968b8cda0f
davdet <dv.dt@hotmail.com>
2022-11-29 09:47:49 +0100

    functional component

--- microfrontends/digital-assistant-config/src/App.js
+++ microfrontends/digital-assistant-config/src/App.js
@@ -1,122 +1,115 @@
-import React, {Component} from 'react';
-import {Buffer} from "buffer";
+import React, { useEffect, useState } from 'react'
 
+const App = (props) => {
+  const [name, setName] = useState('')
+  const [invalid, setInvalid] = useState(false)
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      invalid: false
-    };
-  }
+  useEffect(() => {
+    if (props.name) {
+      console.log('useEffect', props.name)
+      setName(props.name)
+    }
+  }, [props.name])
 
-  componentWillReceiveProps(props) {
-    this.setState({
-      name: props.name,
-    })
-  }
-
-  // componentDidMount() {
-  //   let valore = document.getElementById("name").value;
-  //   if (!valore && valore !== "") {
-  //     this.setState({invalid: false});
-  //     console.log("valore dopo didmount", this.state.invalid);
-  //   }
-  //   console.log("valore dopo didmount 2", this.state.invalid);
-  // }
-
-  handleChange = e => {
-    const input = e.target;
-    const encoded = input.value;
-    const save = Array.from(
-      document.getElementsByClassName('save')
-    );
-    let saveButton = save[0];
-    let decoded;
-
-      decoded = Buffer.from(encoded, 'base64').toString('utf-8');
-      if (encoded !== '' && Buffer.from(decoded, 'utf-8').toString('base64') === encoded) {
-        this.setState({invalid: false});
-        saveButton.removeAttribute('disabled');
-        saveButton.style.pointerEvents = "unset";
-      } else {
-        // invalid input
-        decoded = input.value;
-        this.setState({invalid: true});
-        saveButton.setAttribute('disabled');
-        saveButton.style.pointerEvents = "none";
-      }
-    this.setState({
-      [input.name]: decoded,
-
-    });
-  };
-
-  toBase64 = value => {
-    if ((value) && value !== '') {
-      let encoded = Buffer.from(value, 'utf-8').toString('base64');
+  const toBase64 = (value) => {
+    if (value && value !== '') {
+      let encoded = Buffer.from(value, 'utf-8').toString('base64')
 
       //console.log('presento stringa codificata ' + encoded);
-      return encoded;
+      return encoded
     }
-    return value;
+    return value
   }
 
-  render() {
-    const { name } = this.state;
+  const handleChange = (e) => {
+    const input = e.target
+    const encoded = input.value
+    const save = Array.from(document.getElementsByClassName('save'))
+    let saveButton = save[0]
+    let decoded
 
-    console.log('configurazione pre render', this.state);
+    decoded = Buffer.from(encoded, 'base64').toString('utf-8')
+    if (
+      encoded !== '' &&
+      Buffer.from(decoded, 'utf-8').toString('base64') === encoded
+    ) {
+      setInvalid(false)
+      saveButton.removeAttribute('disabled')
+      saveButton.style.pointerEvents = 'unset'
+    } else {
+      // invalid input
+      decoded = input.value
+      setInvalid(true)
+      saveButton.setAttribute('disabled')
+      saveButton.style.pointerEvents = 'none'
+    }
+    // this.setState({
+    //   [input.name]: decoded,
+    // })
+    setName(decoded)
+  }
 
-    return (
-        <div className="PageConfigPage__panel-body panel-body">
-          <div className="digital-assistant">
-            <h5>
-              <span className="icon fa fa-puzzle-piece" title="Widget"></span>
-              <span> Digital Assistant MFE</span>
-            </h5>
-            <form className="form-horizontal">
-              <div className="row">
-                <div className="col-xs-12">
-                  <fieldset className="no-padding">
-                    <legend role="heading" aria-level="1">
-                      <span>Base64 url setting </span>
-                    </legend>
-                    <div className="no-padding col-lg-12 col-md-12">
-
-                      <div className="form-group">
-                        <div className="text-right mobile-left col-sm-2 col-xs-12">
-                          <label htmlFor="name" className="control-label">
-                            <span className="FormLabel">
-                              <span>Insert valid Base64 code</span>
-                            </span>
-                          </label>
-                        </div>
-                        <div className="col-sm-10 col-xs-12">
-                          <input className="form-control"  id="name" name="name" defaultValue={this.toBase64(name)} type="text" onChange={
-                            this.handleChange}  />
-                        </div>
-                      </div>
+  return (
+    <div className='PageConfigPage__panel-body panel-body'>
+      <div className='digital-assistant'>
+        <h5>
+          <span className='icon fa fa-puzzle-piece' title='Widget'></span>
+          <span> Digital Assistant MFE</span>
+        </h5>
+        <form className='form-horizontal'>
+          <div className='row'>
+            <div className='col-xs-12'>
+              <fieldset className='no-padding'>
+                <legend role='heading' aria-level='1'>
+                  <span>Base64 url setting </span>
+                </legend>
+                <div className='no-padding col-lg-12 col-md-12'>
+                  <div className='form-group'>
+                    <div className='text-right mobile-left col-sm-2 col-xs-12'>
+                      <label htmlFor='name' className='control-label'>
+                        <span className='FormLabel'>
+                          <span>Insert valid Base64 code</span>
+                        </span>
+                      </label>
                     </div>
-
-                    <div className={`labelerror no-padding col-lg-12 ${ this.state.invalid ? 'error' : 'hidden' }`}>
-                      <div className="alert alert alert-danger">
-                        <span aria-hidden="true" className="pficon pficon-error-circle-o"></span>
-                        <span><strong>Insert a valid activation code</strong></span>
-                      </div>
+                    <div className='col-sm-10 col-xs-12'>
+                      <input
+                        className='form-control'
+                        id='name'
+                        name='name'
+                        defaultValue={() => toBase64(name)}
+                        type='text'
+                        onChange={handleChange}
+                      />
                     </div>
-
-                  </fieldset>
+                  </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col-xs-12"></div>
-              </div>
-            </form>
+
+                <div
+                  className={`labelerror no-padding col-lg-12 ${
+                    invalid ? 'error' : 'hidden'
+                  }`}
+                >
+                  <div className='alert alert alert-danger'>
+                    <span
+                      aria-hidden='true'
+                      className='pficon pficon-error-circle-o'
+                    ></span>
+                    <span>
+                      <strong>Insert a valid activation code</strong>
+                    </span>
+                  </div>
+                </div>
+              </fieldset>
+            </div>
           </div>
-        </div>
-    );
-  }
+          <div className='row'>
+            <div className='col-xs-12'></div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
